7ee02eb4b63132cc70cba0f98ce62c5c90dc656d
LyfeTyme <48641165+LyfeTyme@users.noreply.github.com>
2019-06-01 12:10:04 -0700

    First Update on object.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,46 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-
+const tyrannosaurus = {
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceous",
+  roar: function() {
+      return "RAWERSRARARWERSARARARRRR!";
+  }
+}
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const stegosaurus = {
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic"
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const velociraptor = {
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceous"
+}
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(`The Tyrannosaurus weighs ${tyrannosaurus.weight}`);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(`The Velociraptor is ${velociraptor.diet}`);
 
 // How long was a stegosaurus?
-console.log();
+console.log(`The Stegosaurus is ${stegosaurus.length} long`);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(`The Tyrannosaurus lived in the ${tyrannosaurus.period} Period`);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -50,7 +68,14 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+
+function listUni(items) {
+  var universities = [items.university].map;
+  return universities;
+}
+
+universities.sort();
+console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -59,6 +84,7 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
 console.log(contactInfo);
 
 
