db025dbbb71cff0db2c8cdfc5a559522789638c9
LyfeTyme <48641165+LyfeTyme@users.noreply.github.com>
2019-06-03 17:25:29 -0700

    Stretch Running into issues?

--- challenges/classes.js
+++ challenges/classes.js
@@ -20,22 +20,31 @@ class CuboidMaker{
     height: 5
   });
 
+  class CubeMaker extends CuboidMaker{
+    constructor(length, width, height){
+    super(length, width, height)
+    }
+  
+    CubeVol(){
+      return (this.width * this.height * this.length);
+    }
+    CubeSurf(){
+      return 6 * (this.length * this.length );
+    }
+  }
+  const fullCube = new CubeMaker(4,5,5);
+
   console.log(cuboid.cuboidVol()); // 100
   console.log(cuboid.cuboidSurf()); // 130
   
   
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+
+
+console.log(fullCube.CubeVol);
+console.log(fullCube.CubeSurf);
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas 
 // for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
 
-class CubeMaker extends CuboidMaker{
-  constructor(length, width, height)
-  super(length, width, height);
 
-  this.length = length;
-  this.width = width;
-  this.height = height;
-}
